9d172e85d86ad261876330ec9688de705497a59a
Oliver Udo <oliv611m@stud.kea.dk>
2020-12-01 00:38:12 +0100

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import java.io.File;
+;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan({"com.example.demo", "controllers"})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        new File(MainController.uploadDirectory).mkdir();
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
