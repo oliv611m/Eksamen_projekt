1597a5f819b2b23c835eb069fe459f374cc9254e
Oliver Udo <oliv611m@stud.kea.dk>
2020-12-04 14:56:30 +0100

    added repositories

---
+++ src/main/java/com/example/demo/repositories/LoginRepository.java
@@ -0,0 +1,22 @@
+package com.example.demo.repositories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class LoginRepository {
+    private Connection connection;
+
+    public boolean setConnection(){
+        boolean bres = false;
+        String url = "jdbc:mysql://localhost:3306/pkv1?serverTimezone=UTC";
+        try{
+            connection = DriverManager.getConnection(url,"pkv_server","pkv_server");
+            bres = true;
+        }
+        catch (SQLException e){
+            System.out.println("No connection to server="+e.getMessage());
+        }
+        return bres;
+    }
+}
